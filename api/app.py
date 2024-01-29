d024a87c7ebdb7af761737a17d8c3ad62d20078c
shrrgn <mr.shrrgn@gmail.com>
2024-01-29 10:14:07 +0100

    added static html page

--- api/app.py
+++ api/app.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from starlette.requests import Request
 
 from api.config import settings
 from api.routers import api_router
@@ -9,3 +13,16 @@ app = FastAPI(
     description=settings.description,
 )
 app.include_router(api_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def calculator(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
